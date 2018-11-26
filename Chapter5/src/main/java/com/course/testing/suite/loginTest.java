e145bb172192f0b83500a7ff5e6e38fe2b38294a
wangxuhong2 <15010837471@163.com>
2018-11-26 16:47:22 +0800

    代代码

---
+++ Chapter5/src/main/java/com/course/testing/suite/loginTest.java
@@ -0,0 +1,10 @@
+package com.course.testing.suite;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+@Test
+    public void loginTaoBao(){
+        System.out.println("淘宝登录成功");
+    }
+}
